96cc133153910a8e079b97883d0c0f7661650642
yasmine alaya <alayayesmine12@gmail.com>
2024-12-15 18:12:03 +0100

    version finale

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View ,Image, FlatList} from 'react-native';
+import Navigation from './src/Navigation'
+import { Provider } from 'react-redux';
+import store from './src/store/store';
+import ProductsScreen from './src/screens/ProductsScreen';
 export default function App() {
+
   return (
+    <Provider store={store}>
     <View style={styles.container}>
-      <Text>Hello world</Text>
-      <StatusBar style="auto" />
-    </View>
+<Navigation/>
+ <StatusBar style='auto'/>
+     </View>
+     </Provider>
+     
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    justifyContent: 'center',
   },
+  
 });
